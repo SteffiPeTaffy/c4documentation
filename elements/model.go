4d516a247a8b18a1461aa211005848f3d75d497c
Martin Rauscher <martin.rauscher@daimler.com>
2020-07-28 11:43:39 +0200

    #0 SG,MR add model filtering

--- elements/model.go
+++ elements/model.go
@@ -24,3 +24,15 @@ func (m *C4Model) CreateBoundaryView(filter func(element *C4Element) bool) *Boun
 
 	return NewBoundaryView(filteredElements)
 }
+
+func (m *C4Model) Filter(f func(e *C4Element) bool) *C4Model {
+	filteredElements := make([]*C4Element, len(m.Elements))
+	for i := range m.Elements {
+		if f(m.Elements[i]) {
+			filteredElements[i] = m.Elements[i]
+		}
+	}
+	return &C4Model{
+		Elements: filteredElements,
+	}
+}
